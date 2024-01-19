8cb2f5eb18423c6f209a12cc656f28ea647a966a
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-19 10:48:01 -0800

    hoping}

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,3 +5,9 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+# Install werkzeug package with version 2.1.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
