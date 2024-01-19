3a68005895897fdce2ac19d9aafac254ca64a9a3
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-19 10:32:32 -0800

    wahala

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Install flask 2.1.0
+
+exec { 'flask':
+  command => 'sudo apt-get install -y python3-flask=2.1.0',
+  path    => '/usr/bin:/bin'
 }
