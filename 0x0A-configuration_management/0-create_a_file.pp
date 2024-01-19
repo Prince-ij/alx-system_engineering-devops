548efffd7cec3151cdec20780a8019d35245bcfb
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-19 09:05:44 -0800

    all

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 # The following attributes must be in place
 # path  /tmp/school, perm 0744, owner:group www-data, content I love Puppet
 
-file { '/tmp/school:'
+file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
   group   => 'www-data',
