a9ab82100259973e92bb8dfcabfe35b2c0a3a270
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-19 08:36:57 -0800

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# This creates a file in /tmp/school
+# The following attributes must be in place
+# path  /tmp/school, perm 0744, owner:group www-data, content I love Puppet
+
+file { '/tmp/school:'
+  ensure  => file,
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet'
+}
